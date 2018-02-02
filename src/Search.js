86594db43fb7f5ddf0911bd556994eaebacca77c
VijayKapoor92 <vijayk_1@hotmail.com>
2018-02-02 19:50:36 -0200

    feat: Adiciona debounce

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Debounce } from 'react-throttle';
 import PropTypes from "prop-types";
 import { search } from './BooksAPI';
 import Books from './Books';
@@ -71,11 +72,13 @@ class Search extends Component {
             <div className="search-books-bar">
                 <Link className="close-search" to="/" />
                 <div className="search-books-input-wrapper">
-                    <input
-                        type="text"
-                        placeholder="Search by title or author"
-                        onChange={(e) => handleQuery(e.target.value)}
-                    />
+                    <Debounce time="400" handler="onChange">
+                        <input
+                            type="text"
+                            placeholder="Search by title or author"
+                            onChange={(e) => handleQuery(e.target.value)}
+                        />
+                    </Debounce>
                 </div>
             </div>
         );
